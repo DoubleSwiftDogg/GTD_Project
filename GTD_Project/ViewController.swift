d071e5507e8e57017cbd5050c2600502023fae63
MacBook <macbook@macbook-pro-eldorado.local>
2018-10-21 12:47:49 +0300

    Initial Commit

---
+++ GTD_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GTD_Project
+//
+//  Created by MacBook on 21.10.2018.
+//  Copyright © 2018 PB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
